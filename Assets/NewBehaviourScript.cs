2633e53755bff3025e362ca4f9f8b813ecd08bd6
Setta_49 <okk2100@naver.com>
2024-03-16 20:52:37 +0900

    Update NewBehaviourScript.cs

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //하이하이
     }
 
     // Update is called once per frame
